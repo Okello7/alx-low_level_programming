44c6b81090067f34ef8102d46b9751d0a3a940f1
Okello7 <okelloevans07@gmail.com>
2023-06-25 07:07:01 -0700

    Updated the codes for files 4,5,6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,14 @@
 /*C program that prints exact text using printf()*/
 #include <stdio.h>
 
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+
 int main(void)
 {
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,12 @@
-/*
- * C program that prints the size of various types on the computer.
- * Where it is compiled and run on.
- */
-
 #include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 
 int main(void)
 {
@@ -12,13 +15,11 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-	double d;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	printf("size of a double: %ld byte(s)\n", sizeof(d));
 	return (0);
 }
