b21fa4f7ad140fe7dfa1317a439015725fcd4241
Okello7 <okelloevans07@gmail.com>
2023-07-01 08:35:59 -0700

    Update the return function to output the last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@ int print_last_digit(int c)
 {
 	if (c > 0 || c == 0)
 	{
-	_putchar (c % 10 + '0');
-	return (c % 10);
+	n = _putchar (c % 10 + '0');
+	return (n);
 	}
 	else
 	{
 	c = c * -1;
-	_putchar (c % 10 + '0');
+	n =_putchar (c % 10 + '0');
 	return (c % 10);
 	}
 }
